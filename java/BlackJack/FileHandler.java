52313c583dfdee46d7516c0350467fbc0c67b60c
max-crabtree <mtc9567@gmail.com>
2025-03-24 19:42:22 +1100

    Added a new FileHandler class
    Generalises functionality that was repeated in GameFilesManager and RuleFileManager.
    Will refactor code that relied on these classes to use FileHandler instead.

---
+++ java/BlackJack/FileHandler.java
@@ -0,0 +1,135 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileHandler {
+    private static final String STORE_DIR = "store/";
+    private static final String GAMES_DIR = "games/";
+    private static final char DELIMITER = ':';
+
+    private String filename;
+    private File file;
+    private StringBuilder fileString;
+    private StringBuilder[] tokenisedFileString;
+    
+    public FileHandler(String filename) throws IOException {
+        this.filename = filename;
+        this.file = createFile();
+
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        this.fileString = fileToString();
+        this.tokenisedFileString = splitFileAtDelimAndNewline();
+    }
+
+    private File createFile() {
+        File file;
+
+        if (filename.contains("game")) { // maybe change
+            file = new File(STORE_DIR + GAMES_DIR + filename);
+        } else {
+            file = new File(STORE_DIR + filename);
+        }
+
+        return file;
+    }
+
+    private StringBuilder fileToString() {
+        StringBuilder fileStr = new StringBuilder();
+        int c;
+        
+        try {
+            FileReader reader = new FileReader(file);
+            while ((c = reader.read()) != -1) {
+                fileStr.append((char)c);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return fileStr;
+    }
+
+    private StringBuilder[] splitFileAtDelimAndNewline() {
+        String[] tokens = fileString.toString().split(String.format("[%s\n]", DELIMITER));
+        StringBuilder[] keyAndValuePairs = new StringBuilder[tokens.length];
+
+        for (int i = 0; i < tokens.length; i++) {
+            keyAndValuePairs[i] = new StringBuilder(tokens[i].trim());
+        }
+
+        return keyAndValuePairs;
+    }
+
+    private void updateFileContents() throws IOException {
+        FileWriter writer = new FileWriter(file);
+
+        for (int i = 0; i < tokenisedFileString.length; i++) {
+            writer.write(tokenisedFileString[i].toString());
+            if (i % 2 == 0) {
+                writer.write(String.format(" %c ", DELIMITER));   
+            } else {
+                writer.write('\n');
+            }
+        }
+
+        writer.close();
+    }
+
+    public Object getValueOf(String key) {
+        for (int i = 0; i < tokenisedFileString.length; i++) {
+            if (tokenisedFileString[i].toString().equals(key)) {
+                return tokenisedFileString[i+1];
+            }
+        }
+        return null;
+    }
+
+    public void setValueOf(String key, Object newValue) {
+        for (int i = 0; i < tokenisedFileString.length; i++) {
+            if (tokenisedFileString[i].toString().equals(key)) {
+                tokenisedFileString[i+1] = new StringBuilder(newValue.toString());
+                break;
+            }
+        }
+
+        try {
+            updateFileContents();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addKeyValuePair(String key, Object value) throws IOException {
+        FileWriter writer = new FileWriter(file, true);
+
+        writer.write(String.format("\n%s %c %s", key, DELIMITER, value.toString()));
+
+        writer.close();
+    }
+
+    //public void removeKeyValuePair(String key) {
+    //    for (int i = 0; i < tokenisedFileString.length; i++) {
+    //        if (tokenisedFileString[i].toString().equals(key)) {
+    //            tokenisedFileString[i] = new StringBuilder("");
+    //            tokenisedFileString[i + 1] = new StringBuilder("");
+    //            break;
+    //        }
+    //    }
+    //
+    //    try {
+    //        updateFileContents();
+    //    } catch (IOException e) {
+    //        e.printStackTrace();
+    //    }
+    //}
+
+    @Override
+    public String toString() {
+        return fileToString().toString();
+    }
+}
--- java/BlackJack/Main.java
+++ java/BlackJack/Main.java
@@ -1,25 +1,16 @@
+import java.io.IOException;
+
 public class Main {
     private static final String RULES_FILE_NAME = "rules.txt";
-    private static final String STORE_DIR = "store/";
     private static final String PLAYERS_FILE_NAME = "players.txt";
-    private static final String GAMES_DIR = "games/";
 
-    private static RuleFileManager ruleFile = new RuleFileManager(STORE_DIR + RULES_FILE_NAME);
-    private static GameFilesManager gameFiles = new GameFilesManager(STORE_DIR, GAMES_DIR, PLAYERS_FILE_NAME);
-    private static RuleValidator rules = new RuleValidator();
-    private static GameManager gameManager = new GameManager(gameFiles);
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello, World!");
+        
+        FileHandler rulesFile;
+        FileHandler playersFile;
 
-    public static void main(String[] args) {
-        boolean isOkay;
-
-        ruleFile.loadRules();
-
-        isOkay = rules.isValidRules();
-
-        if (!isOkay) {
-            return;
-        }
-
-        gameManager.startMenu();
+        rulesFile = new FileHandler(RULES_FILE_NAME);
+        playersFile = new FileHandler(PLAYERS_FILE_NAME);
     }
 }
